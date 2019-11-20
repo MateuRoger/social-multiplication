a3a17c254bf43386fddb5b73477f040667b2a1f4
Roger Mateu <mateu.roger@gmail.com>
2019-11-20 10:39:15 +0100

    The Load Valance Ribbon configuration has been added.

--- gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
+++ gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -1,13 +1,16 @@
 package microservices.book.gateway;
 
+import microservices.book.gateway.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class GatewayApplication {
 
   public static void main(String[] args) {
